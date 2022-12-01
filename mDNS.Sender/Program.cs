98f79a406ab18ce9e05a1270adacd9cd335221e7
Dasith Wijes <dasiths@hotmail.com>
2022-12-01 12:51:03 +1100

    Add project files.

---
+++ mDNS.Sender/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Common.Logging;
+using Makaretu.Dns;
+
+Console.WriteLine("Multicast DNS spike");
+
+var mdns = new MulticastService();
+
+foreach (var a in MulticastService.GetIPAddresses())
+{
+    Console.WriteLine($"IP address {a}");
+}
+
+mdns.QueryReceived += (s, e) =>
+{
+    var names = e.Message.Questions
+        .Select(q => q.Name + " " + q.Type);
+    Console.WriteLine($"got a query for {String.Join(", ", names)}");
+};
+mdns.AnswerReceived += (s, e) =>
+{
+    var names = e.Message.Answers
+        .Select(q => q.Name + " " + q.Type)
+        .Distinct();
+    Console.WriteLine($"got answer for {String.Join(", ", names)}");
+};
+mdns.NetworkInterfaceDiscovered += (s, e) =>
+{
+    foreach (var nic in e.NetworkInterfaces)
+    {
+        Console.WriteLine($"discovered NIC '{nic.Name}'");
+    }
+};
+
+var sd = new ServiceDiscovery(mdns);
+sd.Advertise(new ServiceProfile("ipfs1", "_ipfs-discovery._udp", 5010));
+sd.Advertise(new ServiceProfile("x1", "_xservice._tcp", 5011));
+sd.Advertise(new ServiceProfile("x2", "_xservice._tcp", 666));
+var z1 = new ServiceProfile("z1", "_zservice._udp", 5012);
+z1.AddProperty("foo", "bar");
+sd.Advertise(z1);
+
+
+mdns.Start();
+Console.ReadKey();
